0262ad5c4937916c7d7e03f728250928c61d30fa
RuduBear <134881527+rudubear@users.noreply.github.com>
2023-07-26 19:16:30 -0400

    using better equality checks

--- js/myscript.js
+++ js/myscript.js
@@ -4,7 +4,7 @@ let fizzBuzzOutput = "";
 
 for (let number = 1; number < input; number++){
     let addOn = number;
-    if (number % 3 == 0 && number %5 == 0){
+    if (number % 3 === 0 && number %5 === 0){
         addOn = "fizzbuzz";
     } else if (number % 3 == 0) {
         addOn = "fizz";
